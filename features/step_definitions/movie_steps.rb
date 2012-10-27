553e77291e3e034afad7d0d0f14ab0a4c4b23261
marekp <marek.paralic@gmail.com>
2012-10-27 12:45:29 +0200

    hw3 partb

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,4 +26,43 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  if (uncheck != nil)
+    #unchecks steps
+    count = 1
+    rating_list.split(%r{,\s*}).each do |ratx|
+      if (count == 1)
+         step %Q{I uncheck "ratings_#{ratx}"}
+         count = 2
+      else
+         step %Q{I uncheck "ratings_#{ratx}"}
+      end
+    end
+  else
+  #check steps
+    count = 1
+    rating_list.split(%r{,\s*}).each do |ratx|
+      if count == 1
+         step %Q{I check "ratings_#{ratx}"}
+         count = 2
+      else
+         step %Q{I check "ratings_#{ratx}"}
+      end
+    end
+  end
 end
+
+Then /^I should see no movie$/ do
+  movies = Movie.all
+  movies.each do |movie|
+    p movie[:title]
+    step %Q{I should not see "#{movie[:title]}"}
+  end
+end
+
+Then /^I should see all of the movies$/ do
+  movies = Movie.all
+  movies.each do |movie|
+    step %Q{I should see "#{movie[:title]}"}
+  end
+end
+
